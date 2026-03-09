126c0a0d3f57af621c5578a5240398d481a01eb6
Артем Яременко <artemyaremenko21@gmail.com>
2026-03-09 18:32:43 +0200

    Аналіз коду

--- Travel_agency.PL/Program.cs
+++ Travel_agency.PL/Program.cs
@@ -112,6 +112,7 @@ builder.Services.AddControllers()
     });
 
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
