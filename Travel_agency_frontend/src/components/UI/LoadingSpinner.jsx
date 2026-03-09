1a306e468c91423ea18913af96e23018f3aa1a92
Yaremenko Artem <artemyaremenko21@gmail.com>
2026-03-09 18:37:38 +0200

    Delete Travel_agency_frontend directory

--- Travel_agency_frontend/src/components/UI/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LoadingSpinner = () => (
-  <div className="flex justify-center items-center p-8">
-    <div className="animate-spin rounded-full h-8 w-8 border-4 border-t-blue-600 border-gray-200"></div>
-  </div>
-)
-
-export default LoadingSpinner
