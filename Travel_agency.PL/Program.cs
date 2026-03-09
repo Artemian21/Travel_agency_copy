e37a9263d94bcdfb1f03e8bfc1d49c42dd235153
Артем Яременко <artemyaremenko21@gmail.com>
2026-03-09 18:20:57 +0200

    The connection string has been changed to LocalConnection In Program.cs, the connection string ‘LocalConnection’ is now used for the TravelAgencyDbContext context instead of “DefaultConnection”. This will allow the application to work with the database specified in ‘LocalConnection’ in the configuration.

--- Travel_agency.PL/Program.cs
+++ Travel_agency.PL/Program.cs
@@ -18,7 +18,7 @@ using Travel_agency.PL.Models.Requests;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TravelAgencyDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); // Use Local or Default connection string 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection"))); // Use Local or Default connection string 
 
 builder.Services.AddScoped<IHotelBookingRepository, HotelBookingRepository>();
 builder.Services.AddScoped<IHotelRepository, HotelRepository>();
